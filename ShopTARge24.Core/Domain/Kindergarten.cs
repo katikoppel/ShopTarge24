89acb86ff0d15ee4f0cd89b8d2edfe625743d88e
katikoppel <katikoppel1995@gmail.com>
2025-09-16 11:16:26 +0300

    KindergartenDomainObject

---
+++ ShopTARge24.Core/Domain/Kindergarten.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopTARge24.Core.Domain
+{
+    internal class Kindergarten
+    {
+        public Guid Id { get; set; }
+        public string GroupName { get; set; }
+        public int ChildrenCount { get; set; }
+        public string KindergartenName { get; set; }
+        public string TeacherName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
