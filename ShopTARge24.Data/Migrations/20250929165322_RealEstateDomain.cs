0699175b1738e0601360c094b3c42568b5206404
katikoppel <katikoppel1995@gmail.com>
2025-09-29 19:55:56 +0300

    RealeEstateMigration

---
+++ ShopTARge24.Data/Migrations/20250929165322_RealEstateDomain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopTARge24.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RealEstateDomain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RealEstates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: true),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RoomNumber = table.Column<int>(type: "int", nullable: true),
+                    BuildingType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RealEstates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RealEstates");
+        }
+    }
+}
--- ShopTARge24.Data/ShopTARge24Context.cs
+++ ShopTARge24.Data/ShopTARge24Context.cs
@@ -13,5 +13,6 @@ namespace ShopTARge24.Data
 
             public DbSet<Spaceships> Spaceships { get; set; }
             public DbSet<FileToApi> FileToApis { get; set; }
+            public DbSet<RealEstate> RealEstates { get; set; }
         }
 }
