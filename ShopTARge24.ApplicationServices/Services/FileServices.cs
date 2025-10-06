21a7bc6742f4569217c73a223443bd1b4085f86e
katikoppel <katikoppel1995@gmail.com>
2025-10-06 08:49:42 +0300

    RealEstateDomain

--- ShopTARge24.ApplicationServices/Services/FileServices.cs
+++ ShopTARge24.ApplicationServices/Services/FileServices.cs
@@ -98,5 +98,10 @@ namespace ShopTARge24.ApplicationServices.Services
 
             return null;
         }
+
+        public void UploadFilesToDatabase(RealEstateDto dto, RealEstate domain)
+        {
+
+        }
     }
 }
