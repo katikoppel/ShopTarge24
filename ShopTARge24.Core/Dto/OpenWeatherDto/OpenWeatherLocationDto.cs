de908512189729f17146e1912ff86c0bfa8dfcbe
katikoppel <katikoppel1995@gmail.com>
2025-11-02 17:43:51 +0200

    OpenWeatherResultDto and OpenWeatherLocationDto created

--- ShopTARge24.Core/Dto/OpenWeatherDto/OpenWeatherLocationDto.cs
+++ ShopTARge24.Core/Dto/OpenWeatherDto/OpenWeatherLocationDto.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace ShopTARge24.Core.Dto.OpenWeatherDto
 {
     public class OpenWeatherLocationDto
     {
-        [JsonPropertyName("name")]
+        [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonPropertyName("lat")]
+        [JsonProperty("lat")]
         public double Latitude { get; set; }
 
-        [JsonPropertyName("lon")]
+        [JsonProperty("lon")]
         public double Longitude { get; set; }
 
-        [JsonPropertyName("country")]
+        [JsonProperty("country")]
         public string Country { get; set; }
 
-        [JsonPropertyName("state")]
+        [JsonProperty("state")]
         public string State { get; set; }
     }
 }
