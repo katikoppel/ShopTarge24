2dfc96b7ee71e739dbe1c04ee925dbf3b194c98c
katikoppel <katikoppel1995@gmail.com>
2025-11-16 18:05:19 +0200

    Revert "Fix single image deletion action"
    This reverts commit f53218815d31646e7afea12f637be73a49035690.

--- ShopTARge24/Controllers/KindergartensController.cs
+++ ShopTARge24/Controllers/KindergartensController.cs
@@ -12,18 +12,15 @@ namespace ShopTARge24.Controllers
     {
         private readonly ShopTARge24Context _context;
         private readonly IKindergartenServices _kindergartenServices;
-        private readonly IFileServices _fileServices;
 
         public KindergartensController
             (
                 ShopTARge24Context context,
-                IKindergartenServices kindergartenServices,
-                IFileServices fileServices
+                IKindergartenServices kindergartenServices
             )
         {
             _context = context;
             _kindergartenServices = kindergartenServices;
-            _fileServices = fileServices;
         }
 
         public IActionResult Index()
@@ -218,20 +215,11 @@ namespace ShopTARge24.Controllers
                 .Select(y => new KindergartenImageViewModel
                 {
                     Id = y.Id,
-                    KindergartenId = y.KindergartenId,
+                    KindergartenId = y.Id,
                     ImageData = y.ImageData,
                     ImageTitle = y.ImageTitle,
                     Image = string.Format("data:image/gif;base64, {0}", Convert.ToBase64String(y.ImageData))
                 }).ToArrayAsync();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> RemoveImage(Guid imageId, Guid kindergartenId)
-        {
-            await _fileServices.RemoveImageFromDB(imageId);
-
-
-            return RedirectToAction(nameof(Update), new { id = kindergartenId });
         }
     }
 }
