1c9813322c587595e18d005a20c8d157acce0a10
katikoppel <katikoppel1995@gmail.com>
2025-11-23 16:51:09 +0200

    KindergartenTests init

---
+++ KindergartenTest/KindergartenTest.cs
@@ -0,0 +1,14 @@
+﻿using ShopTARge24.Core.Dto;
+using ShopTARge24.Core.ServiceInterface;
+
+namespace KindergartenTest
+{
+    public class KindergartenTest : ShopTARge24.KindergartenTest.TestBase
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ KindergartenTest/TestBase.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShopTARge24.ApplicationServices.Services;
+using ShopTARge24.Core.ServiceInterface;
+using ShopTARge24.Data;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using ShopTARge24.KindergartenTest.Macros;
+using ShopTARge24.KindergartenTest.Mock;
+
+namespace ShopTARge24.KindergartenTest
+{
+    public abstract class TestBase
+    {
+        protected IServiceProvider serviceProvider { get; set; }
+
+        protected TestBase()
+        {
+            var services = new ServiceCollection();
+            SetupServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public virtual void SetupServices(IServiceCollection services)
+        {
+            services.AddScoped<IKindergartenServices, KindergartenServices>();
+            services.AddScoped<IFileServices, FileServices>();
+            services.AddScoped<IHostEnvironment, MockIHostEnvironment>();
+
+            services.AddDbContext<ShopTARge24Context>(x =>
+            {
+                x.UseInMemoryDatabase("TEST");
+                x.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            });
+
+            RegisterMacros(services);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        protected T Svc<T>()
+        {
+            return serviceProvider.GetService<T>();
+        }
+
+        private void RegisterMacros(IServiceCollection services)
+        {
+            var macroBaseType = typeof(IMacros);
+
+            var macros = macroBaseType.Assembly.GetTypes()
+                .Where(t => macroBaseType.IsAssignableFrom(t)
+                && !t.IsInterface && !t.IsAbstract);
+
+            foreach (var macro in macros)
+            {
+                services.AddSingleton(macro);
+            }
+        }
+    }
+}
