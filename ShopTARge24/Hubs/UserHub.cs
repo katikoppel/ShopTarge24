44be81ed4162e081da5ffad8982941729edcb91f
katikoppel <katikoppel1995@gmail.com>
2025-12-07 17:01:06 +0200

    Total Users counter added

--- ShopTARge24/Hubs/UserHub.cs
+++ ShopTARge24/Hubs/UserHub.cs
@@ -5,6 +5,19 @@ namespace ShopTARge24.Hubs
     public class UserHub : Hub
     {
         public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
 
         public async Task NewWindowLoaded()
         {
