77b985c030a607022fedf5562f9ed76afd522611
katikoppel <katikoppel1995@gmail.com>
2025-12-07 18:48:50 +0200

    SignalR controller updated for chat

--- ShopTARge24/Controllers/SignalRController.cs
+++ ShopTARge24/Controllers/SignalRController.cs
@@ -2,20 +2,24 @@
 using Microsoft.AspNetCore.SignalR;
 using ShopTARge24.Hubs; 
 
+
 namespace ShopTARge24.Controllers
 {
     public class SignalRController : Controller
     {
         private readonly IHubContext<DeathlyHallowsHub> _deathlyHallowsHub;
 
-        public SignalRController
-            (
-                IHubContext<DeathlyHallowsHub> deathlyHallowsHub
-            )
+        public SignalRController(IHubContext<DeathlyHallowsHub> deathlyHallowsHub)
         {
             _deathlyHallowsHub = deathlyHallowsHub;
         }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
         {
             return View();
         }
