b98e9ba9d840f919b327deb79ec43c0b194cc260
katikoppel <katikoppel1995@gmail.com>
2025-12-07 18:11:19 +0200

    DeathlyHallows update counters

--- ShopTARge24/Controllers/SignalRController.cs
+++ ShopTARge24/Controllers/SignalRController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using ShopTARge24.Hubs; 
 
 namespace ShopTARge24.Controllers
 {
     public class SignalRController : Controller
     {
+        private readonly IHubContext<DeathlyHallowsHub> _deathlyHallowsHub;
+
+        public SignalRController
+            (
+                IHubContext<DeathlyHallowsHub> deathlyHallowsHub
+            )
+        {
+            _deathlyHallowsHub = deathlyHallowsHub;
+        }
         public IActionResult Index()
         {
             return View();
@@ -15,6 +26,12 @@ namespace ShopTARge24.Controllers
             {
                 SD.DeathlyHallowRace[type]++;
             }
+
+            await _deathlyHallowsHub.Clients.All.SendAsync("updateDeathlyHallowCount",
+                SD.DeathlyHallowRace[SD.Cloak],
+                SD.DeathlyHallowRace[SD.Stone],
+                SD.DeathlyHallowRace[SD.Wand]);
+
             return Accepted();
         }
     }
---
+++ ShopTARge24/wwwroot/js/deathlyHallows.js
@@ -0,0 +1,30 @@
+﻿var cloakSpan = document.getElementById("cloakCounter");
+var stoneSpan = document.getElementById("stoneCounter");
+var wandSpan = document.getElementById("wandCounter");
+
+//Create connection
+var connectionDeathlyHallows = new signalR.HubConnectionBuilder()
+    .withUrl("/hubs/deathlyhallows").build();
+
+//Connect to methods that hub invokes aka receive notifications from hub
+connectionDeathlyHallows.on("updateDeathlyHallowCount", (cloak, stone, wand) => {
+    cloakSpan.innerText = cloak.toString();
+    stoneSpan.innerText = stone.toString();
+    wandSpan.innerText = wand.toString();
+})
+
+//Invoke hub methids aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.invoke("NewWindowLoaded", "Kati").then((value) => console.log(value));
+}
+
+//Start connection
+function fulfilled() {
+    //Do something on start
+    console.log("Connection to User Hub Successful");
+}
+function rejected() {
+    // rejected logs
+}
+
+connectionDeathlyHallows.start().then(fulfilled, rejected);
